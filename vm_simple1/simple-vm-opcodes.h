cf9bcdfeb4810b7a9bd908808e91f36d5b7fc1b7
lulz <oxfemale@users.noreply.github.com>
2018-12-21 12:05:03 +0300

    run vm byte file in stack
    run vm byte file in stack

---
+++ vm_simple1/simple-vm-opcodes.h
@@ -0,0 +1,151 @@
+#ifndef SIMPLE_VM_OPCODES_H
+#define SIMPLE_VM_OPCODES_H 1
+
+
+/**
+ * This ENUM stores the values of our opcodes.
+ *
+ * This is less painful to update than having a series of #define instructions,
+ * and ensure that we have unique and incrementing values for the various
+ * opcodes.
+ *
+ * Opcodes are configured in bunches of 16, with instructions broadly
+ * grouped together by class/type.
+ *
+ */
+enum opcode_values {
+
+    /**
+     * Early opcodes.
+     */
+    EXIT = 0,
+    INT_STORE,
+    INT_PRINT,
+    INT_TOSTRING,
+    INT_RANDOM,
+
+    /**
+     * Jump operations.
+     */
+    JUMP_TO = 0x10,
+    JUMP_Z,
+    JUMP_NZ,
+
+    /**
+     * Math operations.
+     */
+    XOR = 0x20,
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    INC,
+    DEC,
+    AND,
+    OR,
+
+    /**
+     * String operations.
+     */
+    STRING_STORE = 0x30,
+    STRING_PRINT,
+    STRING_CONCAT,
+    STRING_SYSTEM,
+    STRING_TOINT,
+
+    /**
+     * Comparison/Test operations.
+     */
+    CMP_REG = 0x40,
+    CMP_IMMEDIATE,
+    CMP_STRING,
+    IS_STRING,
+    IS_INTEGER,
+
+
+    /**
+     * Misc.
+     */
+    NOP = 0x50,
+    STORE_REG,
+
+    /**
+     * PEEK/POKE operations.
+     */
+    PEEK = 0x60,
+    POKE,
+    MEMCPY,
+
+    /**
+     * Stack operations.
+     */
+    STACK_PUSH = 0x70,
+    STACK_POP,
+    STACK_RET,
+    STACK_CALL
+};
+
+
+
+/* 0x00 - 0x0F */
+void op_exit(struct svm *in);
+void op_int_store(struct svm *in);
+void op_int_print(struct svm *in);
+void op_int_tostring(struct svm *in);
+void op_int_random(struct svm *in);
+
+/* 0x10 - 0x1F */
+void op_jump_to(struct svm *in);
+void op_jump_z(struct svm *in);
+void op_jump_nz(struct svm *in);
+
+/* 0x20 - 0x2F */
+void op_xor(struct svm *in);
+void op_or(struct svm *in);
+void op_add(struct svm *in);
+void op_and(struct svm *in);
+void op_sub(struct svm *in);
+void op_mul(struct svm *in);
+void op_div(struct svm *in);
+void op_inc(struct svm *in);
+void op_dec(struct svm *in);
+
+/* 0x30 - 0x3F */
+void op_string_store(struct svm *in);
+void op_string_print(struct svm *in);
+void op_string_concat(struct svm *in);
+void op_string_system(struct svm *in);
+void op_string_toint(struct svm *in);
+
+/* 0x40 - 0x4F */
+void op_cmp_reg(struct svm *in);
+void op_cmp_immediate(struct svm *in);
+void op_cmp_string(struct svm *in);
+void op_is_string(struct svm *in);
+void op_is_integer(struct svm *in);
+
+/* 0x50 - 0x5F */
+void op_nop(struct svm *in);
+void op_reg_store(struct svm *in);
+
+
+/* 0x60 - 0x6F */
+void op_peek(struct svm *in);
+void op_poke(struct svm *in);
+void op_memcpy(struct svm *in);
+
+
+/* 0x70 - 0x7F */
+void op_stack_push(struct svm *in);
+void op_stack_pop(struct svm *in);
+void op_stack_ret(struct svm *in);
+void op_stack_call(struct svm *in);
+
+
+
+/**
+ * Initialization function.
+ */
+void opcode_init(struct svm *cpu);
+
+#endif                          /* SIMPLE_VM_OPCODES_H */
---
+++ vm_simple1/simple-vm.h
@@ -0,0 +1,180 @@
+#ifndef SIMPLE_VM_H
+#define SIMPLE_VM_H 1
+
+
+
+/**
+ * Count of registers.
+ */
+#define REGISTER_COUNT 10
+
+
+#ifndef _Bool
+#define _Bool short
+#define true   1
+#define false  0
+#endif
+
+
+
+/**
+ * A single register.
+ *
+ * Our registers contain a simple union which allows them to store either
+ * a string or an integer.
+ *
+ */
+typedef struct registers {
+    union {
+        unsigned int integer;
+        char *string;
+    } content;
+    enum { INTEGER, STRING } type;
+} reg_t;
+
+
+
+/**
+ * Flags.
+ *
+ * The various mathematical operations (such as add/sub/xor) will set the
+ * Z flag to be true if their result is zero.
+ *
+ * This flag can then be used for the JMP_Z and JUMP_NZ instructions.
+ */
+typedef struct flags {
+    _Bool z;
+} flag_t;
+
+
+/**
+ * This is the signature for an implementation of a bytecode operation.
+ *
+ * Each operation will receive a pointer to the svm_t, containing the
+ * virtual machine reference.
+ *
+ * Note: Forward-declare the struct so we can use it.
+ */
+struct svm;
+typedef void opcode_implementation(struct svm *in);
+
+
+
+/**
+ * The Simple Virtual Machine object.
+ *
+ * All operations relate to this structure, which is allocated
+ * via `svm_new` and freed with `svm_free`.
+ *
+ */
+typedef struct svm {
+    /**
+     * The registers that this virtual machine possesses
+     */
+    reg_t registers[REGISTER_COUNT];
+
+    /**
+     * The flags the CPU contains.
+     */
+    flag_t flags;
+
+    /**
+     * The instruction-pointer.
+     */
+    unsigned int ip;
+
+    /**
+     * The code loaded in the machines RAM, and size of same.
+     */
+    unsigned char *code;
+    unsigned int size;
+
+    /**
+     * The user may define a custom error-handler for when
+     * register type-errors occur, or there is a division-by-zero
+     * error.
+     *
+     * If set this will be stored here, if not a default implementation
+     * will be provided.
+     *
+     */
+    void (*error_handler) (char *msg);
+
+    /**
+     * This is a lookup table which maps opcodes to the appropriate handler.
+     */
+    opcode_implementation *opcodes[256];
+
+    /**
+     * This is the stack for the virtual machine.  There are
+     * only a small number of entries permitted.
+     */
+    int stack[1024];
+
+    /**
+     * The stack pointer which starts from zero and grows upwards.
+     */
+    int SP;
+
+    /**
+     * State - Shouldn't really be here.
+     */
+    _Bool running;
+
+} svm_t;
+
+
+
+/**
+ * Allocate a new virtual machine instance.
+ */
+svm_t *svm_new(unsigned char *code, unsigned int size);
+
+
+/**
+ * Configure a dedicated error-handler.
+ *
+ * The default error-handler will be called if the bytecode tries to
+ * do something crazy, and will merely output a message to the console
+ * and terminate.
+ *
+ * If you wish to handle errors in a GUI system, or similar, you should
+ * setup your own error-handler here.
+ *
+ */
+void svm_set_error_handler(svm_t * cpup, void (*fp) (char *msg));
+
+
+/**
+ * This function is called if there is an error in handling
+ * a bytecode program - such as a mismatched type, or division by zero.
+ *
+ */
+void svm_default_error_handler(svm_t * cpup, char *msg);
+
+
+/**
+ * Dump the virtual machine registers.
+ */
+void svm_dump_registers(svm_t * cpup);
+
+
+/**
+ * Delete a virtual machine.
+ */
+void svm_free(svm_t * cpup);
+
+
+/**
+ *  Main virtual machine execution loop
+ */
+void svm_run(svm_t * cpup);
+
+/**
+ * Run the virtual machine, but only for the specified number
+ * of instructions.  This is useful for fuzzing, etc.
+ */
+void svm_run_N_instructions(svm_t * cpup, int max_instructions);
+
+
+#endif                          /* SIMPLE_VM_H */
